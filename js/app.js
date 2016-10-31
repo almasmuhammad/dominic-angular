6ad73b6e782ee57df5da3015e2bd4cfb200e527b
jpatricksweeney <jpatricksweeney@yahoo.com>
2016-10-31 10:54:47 -0400

    Initial commit of new Angular version of DominicandCompany web site

---
+++ js/app.js
@@ -0,0 +1,30 @@
+var myApp = angular.module('myApp',
+  ['ngRoute', 'firebase'])
+  .constant('FIREBASE_URL', 'https://dominic-and-company.firebaseio.com/');
+
+
+myApp.run(['$rootScope', '$location',
+  function($rootScope, $location) {
+    $rootScope.$on('$routeChangeError',
+      function(event, next, previous, error) {
+        if (error=='AUTH_REQUIRED') {
+          $rootScope.message = 'Sorry, you must log in to access that page';
+          $location.path('/login');
+        } // AUTH REQUIRED
+      }); //event info
+  }]); //run
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'RegistrationController'
+    }).
+    when('/register', {
+      templateUrl: 'views/register.html',
+      controller: 'RegistrationController'
+    }).
+    otherwise({
+      redirectTo: '/login'
+    });
+}]);
---
+++ js/services/authentication.js
@@ -0,0 +1,133 @@
+myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', '$firebaseObject'
+  , '$location', 'FIREBASE_URL'
+  , function ($rootScope, $firebaseAuth, $firebaseObject, $location, FIREBASE_URL) {
+        var config = {
+            apiKey: "AIzaSyBClzC2i_sRSvQdFEGFshGIUyu5lkA3FNY"
+            , authDomain: "dominic-and-company.firebaseapp.com"
+            , databaseURL: "https://dominic-and-company.firebaseio.com"
+            , storageBucket: "dominic-and-company.appspot.com"
+            , messagingSenderId: "711349142119"
+        };
+        firebase.initializeApp(config);
+        const auth = firebase.auth();
+      
+        auth.onAuthStateChanged(function (authUser) {
+            
+            if( authUser ) {
+                console.log('in AuthStateChanged');
+                console.log(authUser);
+                
+                // Clear any errors displayed from previous login attempt
+                $('#login-error').addClass('hide');
+                
+                // USING *APPLY* TO FORCE DIGEST CYCLE and thus update of "currentUser"
+                $rootScope.$apply(function () {
+                    $rootScope.currentUser = authUser;
+                });
+            }
+            else {
+                // No user is currently logged in
+                $rootScope.$apply(function () {
+                    $rootScope.currentUser = '';
+                });
+            }
+        });
+      
+      
+        var myObject = {
+            
+            /* --------------------- Login functionality --------------------- */
+            login: function(user) {
+                auth.signInWithEmailAndPassword(user.email, user.password).then(function (regUser) {
+                    console.log('login success!');
+                    console.log(regUser.uid);
+                    
+                    // Clear any errors displayed from previous login attempt
+                    $("#login-error").text(' ');
+                    $('#login-error').addClass('hide');
+                    
+                    // Retrieve user's first name for display in success modal
+                    firebase.database().ref('/users/' + regUser.uid).once('value').then(function (snapshot) {
+                        console.log('authentication.js - read user data successful');
+                        firstname = snapshot.val().firstname;
+                        var welcomeMessage = 'Welcome ' + firstname + '!';
+                        displaySuccess('Login Successful', welcomeMessage, '');
+                        $("#myName").text(firstname);
+                    });
+                    
+                    // We've successfully logged in, so close the drop-down!
+                    $(".dropdown-toggle").each(function () {
+                        $(this).attr("aria-expanded", false);
+                        $(this).parent("li").removeClass("open");
+                    });
+                    
+                }).catch(function (error) {
+                    console.log('login failed!');
+                    console.log(error.message);
+                    $("#login-error").text(error.message);
+                    $('#login-error').removeClass('hide');
+                });
+            }, //login
+            
+            
+            /* --------------------- Logout functionality --------------------- */
+            logout: function() {
+                console.log('logout success!');
+                auth.signOut();
+                displaySuccess('Logout Successful', 'Please visit us again soon!', '');
+                $("#myName").text('');
+            }, //logout
+            
+            requireAuth: function() {
+                return auth.$requireAuth();
+            }, //require Authentication
+            
+            
+            /* --------------------- Password reset functionality --------------------- */
+            pwReset: function(user) {
+                console.log('in password reset');
+                const promise = auth.sendPasswordResetEmail(user.email);       
+                promise.catch(function(e) { 
+                    displayFail('Password Reset Failed!', e.message);
+                });
+                $('#myModalPasswordReset').modal('hide');
+            }, // password reset request            
+        
+            
+            /* --------------------- Register functionality --------------------- */
+            register: function(user) {
+                console.log('in register new user!');
+                auth.createUserWithEmailAndPassword(user.email, user.password)
+                .then(function(regUser) {
+                    console.log(regUser.uid);
+                    firebase.database().ref('/users/' + regUser.uid).set({
+                        email: user.email,
+                        firstname: user.firstname,
+                        lastname: user.lastname,
+                        createdate: new Date().toDateString(),
+                        regUser: regUser.uid
+                    });
+                    myObject.login(user);
+                }).catch(function(error) {
+                    displayFail('Registration Failed!', error.message);
+                }); //createUser
+                $('#myModalRegistration').modal('hide');
+            } // register
+            
+        };
+        return myObject;
+      
+        function displaySuccess(titleMsg, message, footerMsg) {
+            $('#modalSuccessAlert-title').text(titleMsg);
+            $('#modalSuccessAlert-body').text(message);
+            $('#modalSuccessAlert-footer').text(footerMsg);
+            $('#modalSuccessAlert').modal('show');
+        }
+      
+        function displayFail(titleMsg, error) {
+            console.log(error);
+            $("#modalFailAlert-title").text(titleMsg);
+            $("#modalFailAlert-body").text(error);
+            $('#modalFailAlert').modal('show');
+        }
+}]); //factory
