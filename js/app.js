12e044b3fe7904b31ad7516f4bab592381766947
jpatricksweeney <jpatricksweeney@yahoo.com>
2016-10-31 16:08:07 -0400

    Wired up Profile form tracer-shot

--- js/app.js
+++ js/app.js
@@ -16,13 +16,17 @@ myApp.run(['$rootScope', '$location',
 
 myApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-    when('/login', {
-      templateUrl: 'views/login.html',
-      controller: 'RegistrationController'
-    }).
-    when('/register', {
-      templateUrl: 'views/register.html',
-      controller: 'RegistrationController'
+    //when('/login', {
+    //  templateUrl: 'views/register.html',
+    //  controller: 'RegistrationController'
+    //}).
+    //when('/register', {
+    //  templateUrl: 'views/register.html',
+    //  controller: 'RegistrationController'
+    //}).
+    when('/profile', {
+      templateUrl: 'views/profile.html',
+      controller: 'ProfileFormController'
     }).
     otherwise({
       redirectTo: '/login'
