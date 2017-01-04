c1ac5738760bfbd15c594af3154c73821e4f5187
jpatricksweeney <jpatricksweeney@yahoo.com>
2017-01-04 10:45:10 -0500

    Added new title to Profile page

--- js/controllers/profileform.js
+++ js/controllers/profileform.js
@@ -2,10 +2,10 @@ myApp.controller('ProfileFormController', ['$scope', '$rootScope', '$firebaseAut
                                            
   function ($scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL) {    
       
-   $scope.tinymceOptions = {
-        plugins: 'advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table contextmenu paste code',
-        toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image'
-    };      
+        $scope.tinymceOptions = {
+            plugins: 'advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table contextmenu paste code',
+            toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image'
+        };      
       
         /* ------------------- Load profile data --------------------- */
         console.log('in profile controller!');
